1682386a8c63cd0460d0184487491f701e689318
Fatbrother <kevin92422@gmail.com>
2022-08-25 15:18:52 +0800

    add settings

---
+++ code/settings.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#define LEVEL 1
+#define GRAVITY 1300
+#define SPEED 240
+#define JUMP_SPEED -500
+#define SHOW_HITBOX false
+#define SHOW_FPS false
+enum STATE {DIE, RUN, JUMP, SHIFT};
